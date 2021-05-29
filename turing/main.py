22f8d6422023d245e6561e45a38c8adb85f4c043
Luca1152 <t.luca1152@gmail.com>
2021-05-29 21:01:11 +0300

    Add TM code
    Added the TM code which properly accepts/rejects words.

--- turing/main.py
+++ turing/main.py
@@ -1,59 +1,68 @@
-""" Datele de intrare vor fi:
+from dataclasses import dataclass
+from typing import Dict, Tuple
 
-q0                          <- starea initiala
-qAccept                     <- starea finala
 
-q0,0,qRight0,_,>            <- o deplasare: separate prin ',' avem: starea actuala, caracterul de pe banda, noua stare, ce scrie pe banda (_ inseamna caracterul gol/vid), >/</_ este deplasarea)
+@dataclass
+class TM(object):
+    # States
+    start_state: str
+    end_state: str
 
-qRight0,0,qRight0,0,>
-qRight0,1,qRight0,1,>
-q0,1,qRight1,_,>
-qRight1,0,qRight1,0,>
-qRight1,1,qRight1,1,>
-qRight0,_,qSearch0L,_,<
-qSearch0L,0,q1,_,<
-qRight1,_,qSearch1L,_,<
-qSearch1L,1,q1,_,<
-q1,0,qLeft0,_,<
-qLeft0,0,qLeft0,0,<
-qLeft0,1,qLeft0,1,<
-q1,1,qLeft1,_,<
-qLeft1,0,qLeft1,0,<
-qLeft1,1,qLeft1,1,<
-qLeft0,_,qSearch0R,_,>
-qSearch0R,0,q0,_,>
-qLeft1,_,qSearch1R,_,>
-qSearch1R,1,q0,_,>
-qSearch0R,1,qReject,1,-
-qSearch1R,0,qReject,0,-
-qSearch0L,1,qReject,1,-
-qSearch1L,0,qReject,0,-
-q0,_,qAccept,_,-
-q1,_,qAccept,_,-
-qSearch0L,_,qAccept,_,-
-qSearch0R,_,qAccept,_,-
-qSearch1L,_,qAccept,_,-
-qSearch1R,_,qAccept,_,-
+    transitions: Dict[Tuple[str,], Tuple[str, ...]]
+    tape: Dict[int, str]
 
-"""
 
-# TODO iar programul ar trebui sa citeasca datele si dupa oricate cuvinte diferite si sa afiseze
-#   daca sunt validate sau nu. Spre exemplu:
-"""
+def read_tm(file_path) -> TM:
+    with open(file_path) as file:
+        start_state = file.readline().strip()
+        end_state = file.readline().strip()
 
-Ati citit masina Turing cu configurarea ... (si sa afisati datele citite aici)
-Puteti introduce cuvinte pe care sa le testeze MT :).
+        file.readline()
 
-cuvant: 0110
-OK
-cuvant: 001
-not OK..
-cuvant: 11100110111
-OK
-cuvant: 01
-not OK..
+        transitions_dict = {}
+        while line := file.readline():
+            transition_list = line.strip().split(",")
+            transitions_dict[tuple(transition_list[0:2])] = tuple(transition_list[2:])
 
-"""
+        return TM(start_state, end_state, transitions_dict, {})
 
-# TODO me: make a configuration that uses left side of the tape
-# sa impart punctaj inre assignemnturile deja puse?
+
+def is_accepted(tm: TM, word: str):
+    tm.tape = {}
+    for index, letter in enumerate(word):
+        tm.tape[index] = letter
+
+    current_index = 0
+    current_state = tm.start_state
+
+    while (True):
+        # The tape (which is a dict) is too short to fit in the current index. Place the empty symbol.
+        if current_index not in tm.tape:
+            tm.tape[current_index] = "_"
+
+        # Accepted condition
+        if current_state == tm.end_state:
+            return True
+
+        # There are no more moves from the current (state, symbol) pair
+        if (current_state, tm.tape[current_index]) not in tm.transitions:
+            return False
+
+        new_state, new_letter, direction = tm.transitions[(current_state, tm.tape[current_index])]
+        tm.tape[current_index] = new_letter
+        if direction == ">":
+            current_index += 1
+        elif direction == "<":
+            current_index -= 1
+        current_state = new_state
+
+
+def main():
+    tm = read_tm('input.txt')
+
+    for word in ["0110", "001", "111001100111", "01"]:
+        print(f"'{word}' -> {is_accepted(tm, word)}")
+
+
+if __name__ == '__main__':
+    main()
