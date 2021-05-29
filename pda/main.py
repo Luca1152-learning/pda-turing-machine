c6ab2a61c9130d8e29dda66b3a08b3ae650bf097
Luca1152 <t.luca1152@gmail.com>
2021-05-29 19:16:49 +0300

    Add PDA code
    Added the PDA code that solves all TODOs.

--- pda/main.py
+++ pda/main.py
@@ -1,81 +1,29 @@
 from dataclasses import dataclass
-import queue
-
-"""
-PDA
-
-Cum arata fisierul:
-
-#           <- caracterul pt lambda
-S           <- starea initiala
-A B C       <- starile
-C           <- starile finale
-a           <- alfabetul
-ASZ         <- alfabetul stivei
-Z           <- caracterul de la baza stivei
-
-            <- tranzitiile:
-S A a Z/ASZ    <-   Stare de unde se deplaseaza, noua stare, litera citita din cuvant, cum modifica stiva
-A A a A/AA    <-   Stare de unde se deplaseaza, noua stare, litera citita din cuvant, cum modifica stiva
-
-A B # A/A    <-   Stare de unde se deplaseaza, noua stare, litera citita din cuvant, cum modifica stiva
-B B # AAA/A
-B C # SZ/Z
-
-
-
-<- si vom valida doar cand si stiva ramane goala
-<- cu stare finala
-<- cu ambele
-
-
-1. Cititi PDA si salvati-l intr-o structura utila
-    (spre exemplu folosim @dataclass cu typing "ca lumea")
-2.
-
-
-"""
-# TODO Ce tip de dictionar ne-ar fi util ca sa verificam daca putem trece de la o stare la alta stare? Stim stiva si litera pe care o citeste PDA-ul.
-
-# Deci facem ca la DFA, avand doua stari poate exista o singura tranzitie cu o anume litera citita
-# adica (stare1, stare2, litera) formeaza o cheie, iar valoarea este un tuplu de cuvinte din alfabetul stivei, adica ce sa citeasca si ce sa scrie in stive
-
-"""
-{
-    ('A','A,'a') : ("A", "A"),
-    ('B','C,'#') : ("SZ", "Z"),
-    ...
-}
-"""
-
 from typing import List, Dict, Tuple
 
 
 @dataclass
 class PDA(object):
+    # Chars
     LAMBDA: str
+    STACK_BASE: str
 
+    # States
     start_state: str
     states: List[str]
     end_states: List[str]
 
+    # Alphabets
     alpha: str
     stack_alpha: str
 
-    """
-    {
-        ('A','A','a') : ("A", "AA"),
-        ('B','C,'#') : ("Z", "Z"),
-        ...
-    }
-    """
+    # Dictionary with states + stack updates
     d: Dict[Tuple[str, ...], Tuple[str, ...]]
 
-    STACK_BASE: str
 
-
-def read_PDA(infile):
+def read_pda(infile):
     with open(infile) as file:
+        # Read the values
         LAMBDA = file.readline().strip()
 
         start = file.readline().strip()
@@ -87,8 +35,8 @@ def read_PDA(infile):
 
         STACK_BASE = file.readline().strip()
 
+        # Create the dictionary
         d = dict()
-        # while line := file.readline().strip():
         for line in file.readlines():
             line = line.strip()
 
@@ -98,7 +46,7 @@ def read_PDA(infile):
             key, value = tuple(components[: 3]), tuple(components[3].split('/'))
             d[key] = value
 
-    return PDA(LAMBDA, start, states, end_states, alpha, stack_alpha, d, STACK_BASE)
+    return PDA(LAMBDA, STACK_BASE, start, states, end_states, alpha, stack_alpha, d)
 
 
 @dataclass
@@ -109,82 +57,80 @@ class Snapshot(object):
 
 
 def can_read_stack_top(stack: List[str], to_read: str):
-    # TODO modificam sa "faca peek" la un singur caracter
     stack_cpy = [item for item in stack]
-    for letter in to_read:
-        stack_top = stack_cpy.pop(-1)
-        if stack_top != letter:
-            return False
+
+    stack_top = stack_cpy.pop(-1)
+
+    # The symbol is at the stack's top
+    if stack_top != to_read:
+        return False
+
+    # The symbol is at the stack's top
     return True
 
 
 def read_stack_top(stack: List[str], to_read: str) -> List[str]:
-    # TODO modificam sa citeasca un singur caracter
     stack_cpy = [item for item in stack]
-    for letter in to_read:
-        stack_top = stack_cpy.pop(-1)
-        if stack_top != letter:
-            return None
+
+    stack_top = stack_cpy.pop(-1)
+    if stack_top != to_read:
+        return None
+
     return stack_cpy
 
-# TODO  sa gasiti o modalitate de tratare diferita a tipurilor de validare (doar stare finala / stare finala si stiva goala)
 
-def is_accepted(pda: PDA, word: str):
-    stare_finala, stiva_vida = False, False
+def is_accepted(pda: PDA, word: str, check_final_state, check_empty_stack):
+    reached_final_state, reached_empty_stack = False, False
 
-    # un snapshot are cuvantul (ce a ramasa de citit), are starea curenta, si are si o copie a stivei
-
-    # stack = []
     queue = [
-        # la inceput, avem tot cuvantul de input, suntem in starea initiala si stiva contine doar caracterul de la baza stivei
-        Snapshot(word, pda.start_state, [pda.STACK_BASE])  # Descriere instantanee
+        Snapshot(word, pda.start_state, [pda.STACK_BASE])
     ]
 
     while len(queue) > 0:
-
         top_snap = queue.pop()
-        # TODO am ajuns in stare finala?
+
+        # The word is empty
         if len(top_snap.word) == 0:
-            # validarea 1:
-            # am ajuns intr-o stare finala dupa ce am citit intreg cuvantul?
+            # Check 1: Is the current state final?
             if top_snap.state in pda.end_states:
-                print('Validare 1 (stare finala, stiva nu neaparat goala)')
-                stare_finala = True
-                # verificam daca stiva a e goala
-                if len(top_snap.stack) == 1 and top_snap.stack[0] == pda.STACK_BASE:  # doar 'Z'
-                    stiva_vida = True
-                    # PUTEM intrerupe inclusiv tot algoritmul, deoarece mai bine de atata nu exista :)
-                    return True, True
+                reached_final_state = True
 
-        # vom cauta deplasari posibile (adica.. avand urmatoare litera, sau lambda, cum se poate modifica PDA-ul?)
-        state1 = top_snap.state  # 'S' la primul pas
+            # Check 2: Is the stack empty? (Is 'Z' the only element in the stack?)
+            if len(top_snap.stack) == 1 and top_snap.stack[0] == pda.STACK_BASE:
+                reached_empty_stack = True
 
-        if len(top_snap.word) == 0:  # chiar si daca nu mai ai litere, poate mai exista lambda-tranzitii, unde cuvantul este '#'
+            if (reached_final_state >= check_final_state and reached_empty_stack >= check_empty_stack):
+                return True
+
+        # Search for possible movements for the top snapshot
+        state1 = top_snap.state  # Would be 'S' at the first step
+
+        # Get the current letter
+        if len(top_snap.word) == 0:
+            # Even if the word doesn't have any more letters, maybe there are λ-transitions from the current state
             letter = pda.LAMBDA  # '#'
         else:
+            # The word is not empty, so use its first letter (the word is read from left to right)
             letter = top_snap.word[0]
 
-        stack = top_snap.stack  # ['Z'] la primul pas
+        # Get the current stack
+        stack = top_snap.stack  # ['Z'] initially
 
-
+        # Will contain future snapshots. Possibly more than one, due to PDAs' nondeterministic nature
         next_snaps = []
 
-        for state2 in pda.states:  # cautam noi in d o cheie (state1, ?state2?, letter)
-
+        for state2 in pda.states:
+            # Keys to search for
             d_key = (state1, state2, letter)
-            # cautam si lambda tranzitii
             d_lambda_key = (state1, state2, pda.LAMBDA)
 
-            # pt chei care nu contin lambda
+            # If the d_key and d_lambda_key are not the same (as to not check them twice)
             if d_key != d_lambda_key and pda.d.__contains__(d_key):
-                # valoarea cheiei spune cum trebuie sa modificam stiva.
-                # TODO to_read va trebui sa fie in topul stivei ca sa ne putem deplasa
-                #   iar in mod pozitiv, vom inlocui topul stivei cu to_write
                 to_read, to_write = pda.d[d_key]
+
                 # check if we can remove the specified letters ('A')
                 if can_read_stack_top(stack, to_read):
                     new_stack = read_stack_top(stack, to_read)  # stiva fara 'A'
-                    # for letter in to_write[::-1]:
                     for letter in reversed(to_write):
                         new_stack.append(letter)
 
@@ -193,14 +139,11 @@ def is_accepted(pda: PDA, word: str):
                     )
 
             if pda.d.__contains__(d_lambda_key):
-                # valoarea cheiei spune cum trebuie sa modificam stiva.
-                # to_read ar trebui sa gasim in elementele dinspre varf,
-                #   iar in mod pozitiv, vom inlocui cu to_write
                 to_read, to_write = pda.d[d_lambda_key]
-                # check if we can remove the specified letters ('A')
+
+                # Check if we can remove the specified letters ('A')
                 if can_read_stack_top(stack, to_read):
                     new_stack = read_stack_top(stack, to_read)  # stiva fara 'A'
-                    # for letter in to_write[::-1]:
                     for letter in reversed(to_write):
                         new_stack.append(letter)
 
@@ -208,32 +151,17 @@ def is_accepted(pda: PDA, word: str):
                         Snapshot(top_snap.word, state2, new_stack)  # in order: 'a', 'A'
                     )
 
-        print()
         for snap in next_snaps:
             queue.append(snap)
-            print(top_snap, '->', snap)
-        print()
 
-        # TODO test if the conditions for accepting/rejecting the word are met
-
-    return stare_finala, stiva_vida
-
-"""
-TODO pt  +4p
-1. citirea de pe stiva este implementata ca si cum citeste mai multe caractere la un pas. Trebuie sa faceti cu citirea unui singur caracter.
-2. sa gasiti o alta modalitate de tratare diferita a tipurilor de validare (doar stare finala / stare finala si stiva goala)
-"""
+    # No 'return True' was reach so far, all snapshots were checked, so the word is not in accepted by the PDA
+    return False
 
 def main():
-    pda = read_PDA('input.txt')
+    pda = read_pda('input.txt')
 
-    print(pda)
-
-    """
-    
-    """
-    for word in ['aa', 'a', 'aaaaaaaaaa', '']:  # TODO treat empty str ''
-        print(word,  is_accepted(pda, word))
+    for word in ['aa', 'a', 'aaaaaaaaaa', '']:
+        print(f"'{word}' -> {is_accepted(pda, word, True, True)}")
 
 
 if __name__ == '__main__':
